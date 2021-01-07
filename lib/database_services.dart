94e89c5bd820a51e05155db8c0ae5e7c8bac9cf0
dewangga-pk <pacetcommunity570@gmail.com>
2021-01-07 09:30:20 +0700

    setting db auth firestore

---
+++ lib/database_services.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseServices {
+  static CollectionReference cityCollection =
+      FirebaseFirestore.instance.collection("city");
+
+  static Future<void> createOrUpdateCity(String id, {String name}) async {
+    await cityCollection.doc(id).set({'name': name});
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:feis_mobile/auth_services.dart';
+// import 'package:feis_mobile/database_services.dart';
 import 'package:feis_mobile/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -20,5 +21,67 @@ class MyApp extends StatelessWidget {
         home: Wrapper(),
       ),
     );
+    // var city = [
+    //   "Kab Bangkalan",
+    //   "Kab Banyuwangi",
+    //   "Kab Blitar",
+    //   "Kab Bojonegoro",
+    //   "Kab Bondowoso",
+    //   "Kab Gresik",
+    //   "Kab Jember",
+    //   "Kab Jombang",
+    //   "Kab Kediri",
+    //   "Kab Lamongan",
+    //   "Kab Lumajang",
+    //   "Kab Madiun",
+    //   "Kab Magetan",
+    //   "Kab Malang",
+    //   "Kab Mojokerto",
+    //   "Kab Nganjuk",
+    //   "Kab Ngawi",
+    //   "Kab Pamekasan",
+    //   "Kab Pacitan",
+    //   "Kab Pasuruan",
+    //   "Kab Ponorogo",
+    //   "Kab Probolinggo",
+    //   "Kab Sampang",
+    //   "Kab Sidoarjo",
+    //   "Kab Situbondo",
+    //   "Kab Sumenep",
+    //   "Kab Trenggalek",
+    //   "Kab Tuban",
+    //   "Kab Tulungaung",
+    //   "Kota Batu",
+    //   "Kota Blitar",
+    //   "Kota Kediri",
+    //   "Kota Madiun",
+    //   "Kota Malang",
+    //   "Kota Mojokerto",
+    //   "Kota Pasuruan",
+    //   "Kota Probolinggo",
+    //   "Kota Surabaya"
+    // ];
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: Scaffold(
+    //     body: Container(
+    //       child: Center(
+    //         child: Column(
+    //           mainAxisAlignment: MainAxisAlignment.center,
+    //           children: [
+    //             RaisedButton(
+    //                 child: Text("Add data"),
+    //                 onPressed: () {
+    //                   for (var i = 0; i < 38; i++) {
+    //                     DatabaseServices.createOrUpdateCity((i + 1).toString(),
+    //                         name: city[i]);
+    //                   }
+    //                 }),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
